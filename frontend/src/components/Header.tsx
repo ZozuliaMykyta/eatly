73e737e0839b9ce41dec6613bd9b61c49b4ea55d
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-03-23 12:11:43 +0100

    stylized header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,47 +5,64 @@ const Header = () => {
   const menuList = [
     {
       id: 1,
-      link: "Menu",
+      link: "menu",
     },
     {
       id: 2,
-      link: "Blog",
+      link: "blog",
     },
     {
       id: 3,
-      link: "Pricing",
+      link: "pricing",
     },
     {
       id: 4,
-      link: "Contact",
+      link: "contact",
     },
   ];
   return (
     <header className="header">
-      <div className="header__container container">
-        <Link className="header__logo" href="/">
-          <Image
-            src="/assets/img/favicon.svg"
-            alt="eatly"
-            width={46}
-            height={42}
-            priority
-          />
-          eatly
-        </Link>
-        <ul className="header__list">
-          {menuList.map((item) => (
-            <li className="header__item" key={item.id}>
-              <Link href={`/${item.link.toLowerCase()}`}>{item.link}</Link>
-            </li>
-          ))}
-        </ul>
-        <div className="header__authorization">
-          <Link href="#!" className="header__login">
-            Login
+      <div className="pt-[47px]! pb-[25px]! container flex justify-between border-b-1 border-[#cbcbcb]">
+        <div className="flex gap-[50px] sm:gap-[102px] ">
+          <Link className="flex items-center gap-[11px]" href="/">
+            <Image
+              src="/assets/img/favicon.svg"
+              alt="eatly"
+              width={46}
+              height={42}
+              priority
+            />
+            <span className="text-[22px] leading-[1.3] text-purple font-semibold">
+              eatly
+            </span>
           </Link>
-          <Link href="#!" className="header__singup">
-            Sing Up
+          <ul className="flex items-center gap-[54px]">
+            {menuList.map((item) => (
+              <li key={item.id}>
+                <Link
+                  className="relative text-[18px] text-gray leading-[26px] font-inter font-medium capitalize hover:text-purple transition-all duration-300 group"
+                  href={`/${item.link.toLowerCase()}`}
+                >
+                  <span className="absolute block w-0 h-0.5 bg-purple bottom-[-5px] left-0 transition-all duration-300 group-hover:w-full"></span>
+                  {item.link}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex gap-x-[36px] items-center">
+          <Link
+            href="#!"
+            className="relative text-[18px] text-gray leading-[26px] font-bold capitalize hover:text-purple transition-all duration-300 group"
+          >
+            <span className="absolute block w-0 h-0.5 bg-purple bottom-[-5px] left-0 transition-all duration-300 group-hover:w-full"></span>
+            login
+          </Link>
+          <Link
+            href="#!"
+            className="text-[18px] leading-[26px] font-bold capitalize rounded-[18px] bg-purple text-lighwhite py-[17px] px-[25px] border-2 border-purple transition-all duration-300 hover:bg-white hover:text-purple"
+          >
+            sing up
           </Link>
         </div>
       </div>
