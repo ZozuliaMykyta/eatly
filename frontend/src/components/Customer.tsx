f730781f08034ba297ff8c0e0a97fbbc2b323c10
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-05-04 15:19:32 +0200

    Finished slider

--- frontend/src/components/Customer.tsx
+++ frontend/src/components/Customer.tsx
@@ -2,9 +2,11 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
+import customer from "@/assets/img/home/customer.jpeg";
+import quotes from "@/assets/img/icons/quotes.svg";
 
 const Customer = () => {
-  const [activeSlide, setActiveSlide] = useState<number>();
+  const [activeSlide, setActiveSlide] = useState<number>(0);
   const review =
     "“ Online invoice payment helps companies save time, are faster and save maximum effort for the clients and save maximum effort. Online invoice payment helps companies save time ”";
 
@@ -21,18 +23,41 @@ const Customer = () => {
         </h3>
         <Swiper
           spaceBetween={50}
-          slidesPerView={2.3}
+          slidesPerView={2}
           onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
-          className="mt-[80px]"
+          className="mt-[80px] !overflow-visible"
+          grabCursor={true}
         >
-          {[...Array(5)].map((item, index) => (
+          {[...Array(5)].map((_, index) => (
             <SwiperSlide
-              // className={`bg-amber-400 ${
-              //   activeSlide === index ? "bg-amber-950" : ""
-              // }`}
-              className="rounded-[20px]bg-white px-[43px] py-[40px] shadow-2xl"
+              className="rounded-[20px] bg-white px-[43px] py-[40px] shadow-2xl"
               key={index}
             >
+              {(index === activeSlide ||
+                (activeSlide === 5 - 2 && index === 5 - 1)) && (
+                <div className="mb-[32px] flex items-center justify-between">
+                  <div className="flex items-center gap-[20px]">
+                    <Image
+                      src={customer}
+                      alt={reviewSliderData.name}
+                      width={71.04}
+                      height={71.04}
+                    ></Image>
+                    <div>
+                      <h4>{reviewSliderData.name}</h4>
+                      <h6 className="capitalize">
+                        {reviewSliderData.date} year with us
+                      </h6>
+                    </div>
+                  </div>
+                  <Image
+                    src={quotes}
+                    alt="quotes"
+                    width={58}
+                    height={44}
+                  ></Image>
+                </div>
+              )}
               <p className="mb-[40px] italic text-[18px] fomt-normal leading-[26px] text-[rgb(99,99,99)]">
                 {reviewSliderData.review}
               </p>
