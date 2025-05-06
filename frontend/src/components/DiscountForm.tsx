0a52f973736f93db944058a35c878082cf881089
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-05-06 17:15:06 +0200

    added a basic react-hook-form

--- frontend/src/components/DiscountForm.tsx
+++ frontend/src/components/DiscountForm.tsx
@@ -1,10 +1,23 @@
+"use client";
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface Inputs {
+  email: string;
+}
 
 const DiscountForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex gap-2">
       <input type="email" />
-      <button>subscribe</button>
+      <button type="submit">subscribe</button>
     </form>
   );
 };
