0fc57bbc6329239f75e7f25b1bc616156cde41ca
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-05-09 13:38:47 +0200

    discount section

--- frontend/src/components/Discount.tsx
+++ frontend/src/components/Discount.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import DiscountForm from "./DiscountForm";
+import Image from "next/image";
+import discountImage from "@/assets/img/discount-food.png";
+import discountBg from "@/assets/img/icons/discount-bg.svg";
 
 const Discount = () => {
   return (
-    <section>
-      <div className="container">
-        <div>
-          <h2 className="uppercase">get 50%</h2>
+    <section className="mb-[170px] mt-[130px] min-[768px]:mt-[220px]">
+      <div className="relative container text-center min-[900px]:text-left">
+        <div className="relative overflow-hidden bg-[rgb(108,95,188)] rounded-[30px] pt-[36px] pb-[60px] px-[68px]">
+          <h2 className="relative z-10 uppercase text-[36px] leading-[62px] sm:text-[50px] sm:leading-[75px] min-[900px]:text-[70px] font-extrabold min-[900px]:leading-[105px] text-white mb-[9px]">
+            get 50%
+          </h2>
           <DiscountForm />
+          <div className="hidden min-[768px]:block absolute top-[-50px] right-[20px] left-[20px] bottom-0">
+            <Image src={discountBg} alt="discount background" />
+          </div>
         </div>
+        <Image
+          className="absolute bottom-[-120px] min-[500px]:bottom-[-210px] min-[900px]:bottom-[-60px] right-[50%] max-[900px]:translate-x-[50%] min-[900px]:right-[70px] max-[500px]:w-[200px]"
+          src={discountImage}
+          alt="food"
+        />
       </div>
     </section>
   );
--- frontend/src/components/DiscountForm.tsx
+++ frontend/src/components/DiscountForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface Inputs {
@@ -11,13 +11,51 @@ const DiscountForm = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
+  });
   const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => console.log(data);
 
+  const [isWide, setIsWide] = useState(false);
+  useEffect(() => {
+    const checkWidth = () => setIsWide(window.innerWidth > 600);
+    checkWidth();
+    addEventListener("resize", checkWidth);
+    return () => removeEventListener("resize", checkWidth);
+  }, []);
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex gap-2">
-      <input type="email" />
-      <button type="submit">subscribe</button>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="relative flex gap-2 mb-[50px] z-10 justify-center min-[900px]:justify-start"
+    >
+      <div className="flex flex-col">
+        <div className="relative">
+          <input
+            className="focus:outline-purple w-[250px] min-[500px]:w-[400px] bg-white pl-3.5 min-[500px]:pl-[22px] py-4 min-[500px]:py-[22px] pr-[110px] min-[500px]:pr-[140px] rounded-[14px] text-[10px] min-[500px]:text-[16px] font-normal text-black leading-6 tracking-[3%] placeholder:capitalize placeholder:text-[rgb(135,135,135)]"
+            style={{ boxShadow: "0px 7.25px 45.33px 0px rgba(0, 0, 0, 0.1)" }}
+            type="email"
+            placeholder={isWide ? "enter your email address" : "email address"}
+            {...register("email", {
+              required: true,
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Invalid email address",
+              },
+            })}
+          />
+          <button
+            type="submit"
+            className="focus:outline-purple absolute translate-y-[-50%] top-[50%] right-1.5 cursor-pointer text-[10px] min-[500px]:text-[14px] leading-[14px] min-[500px]:leading-[22px] text-[rgb(247,248,250)] py-2.5 min-[500px]:py-4 px-4 min-[500px]:px-6 rounded-[11px] bg-purple"
+          >
+            subscribe
+          </button>
+        </div>
+        {errors.email && (
+          <div className="text-red-500">{errors.email.message}</div>
+        )}
+      </div>
     </form>
   );
 };
