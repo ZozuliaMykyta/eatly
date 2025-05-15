d3726ed89209f30a848b9a75d5b8bfd3270a1d75
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-05-15 16:10:58 +0200

    add the auth. page

--- frontend/src/app/SignUp/page.tsx
+++ frontend/src/app/SignUp/page.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <div className="container">1</div>
+      <div className="relative container">
+        <Image
+          src="/assets/img/favicon.svg"
+          alt="ealty's logo"
+          width={46}
+          height={42}
+        ></Image>
+      </div>
     </>
   );
 };
