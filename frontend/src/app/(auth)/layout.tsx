c77592143354a9fd735b1dbbe3a7071626d776e7
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-05-15 16:10:59 +0200

    Refactor: move auth and dashboard pages into separate folders(groups)

---
+++ frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Poppins, Inter, Manrope, Roboto } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--font-poppins",
+});
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-inter",
+});
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-manrope",
+});
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} ${inter.variable} ${manrope.variable} ${roboto.variable} font-poppins font-semibold`}
+      >
+        <div className="wrapper">
+          <main className="flex-grow">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins, Inter, Manrope, Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import StoreProvider from "./StoreProvider";
+import StoreProvider from "../StoreProvider";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
