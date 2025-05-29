29fe5a5cfbd55d53eec3883913a1d398907c5cca
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-05-29 16:55:35 +0200

    upgraded handleError

--- backend/handleError.ts
+++ backend/handleError.ts
@@ -1,12 +1,17 @@
 import type { Request, Response } from "express";
 
-const handleError = (res: Response, err: string, errorDetails?: unknown) => {
+const handleError = (
+  res: Response,
+  err: string,
+  errorDetails?: unknown,
+  number: number = 500
+) => {
   if (errorDetails instanceof Error) {
     console.error(errorDetails.message, errorDetails.stack);
   } else {
     console.error(errorDetails);
   }
-  res.status(500).json({ error: err, details: errorDetails });
+  res.status(number).json({ error: err, details: errorDetails });
 };
 
 export default handleError;
