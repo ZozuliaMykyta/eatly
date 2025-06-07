c6a9f72a3431bb9cc1eb0a41b47a2861a0000173
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-06-07 21:02:50 +0200

    added styles

--- frontend/src/app/restaurants/[slug]/RestaurandCard.tsx
+++ frontend/src/app/restaurants/[slug]/RestaurandCard.tsx
@@ -1,15 +1,41 @@
 "use client";
 import { useGetRestaurantsQuery } from "@/lib/services/api";
+import Image from "next/image";
 import React from "react";
 
 const RestaurandCard = ({ slug }: { slug: string }) => {
   const { data, error, isLoading } = useGetRestaurantsQuery();
   const restaurant = data?.find((item) => item._id === slug);
 
-  if (!restaurant) {
-    return <div>Not found</div>;
+  if (isLoading) {
+    return (
+      <div className="container text-center custom-border-bx">
+        <h5 className="mt-5">Loading..</h5>
+      </div>
+    );
   }
-  return <div>{restaurant.theme}</div>;
+  if (error) {
+    return (
+      <div className="container text-center custom-border-b">
+        <h5 className="mt-5">Something went wrong</h5>
+      </div>
+    );
+  }
+  if (!restaurant) {
+    return <div>The restaurant not found</div>;
+  }
+  return (
+    <div className="container">
+      <div className="relative w-full max-w-[800px] h-[200px]">
+        <Image
+          src={restaurant.img}
+          alt={restaurant.title}
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RestaurandCard;
--- frontend/src/app/restaurants/layout.tsx
+++ frontend/src/app/restaurants/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import "../globals.css";
 import StoreProvider from "../StoreProvider";
 
 export const metadata: Metadata = {
