dd3eb0196835805dd6c85bf81befd641f66a6050
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-06-11 09:52:44 +0200

    made the Popular responsive

--- frontend/src/components/restaurants/Popular.tsx
+++ frontend/src/components/restaurants/Popular.tsx
@@ -3,8 +3,8 @@ import DishesCards from "../DishesCards";
 
 const Popular = () => {
   return (
-    <div className="container !mt-[150px]">
-      <h3 className="text-[35px] font-semibold leading-[25px] text-[rgb(50,49,66)]">
+    <div className="container !mt-[100px] md:!mt-[150px]">
+      <h3 className="text-[24px] sm:text-[35px] font-semibold leading-[18px] sm:leading-[25px] text-[rgb(50,49,66)]">
         Popular 🔥
       </h3>
       <DishesCards />
