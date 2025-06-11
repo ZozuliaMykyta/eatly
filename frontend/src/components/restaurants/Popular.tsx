5ffec47e78ff9351ace21137bee5028fb0177f49
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-06-11 10:04:56 +0200

    added the border

--- frontend/src/components/restaurants/Popular.tsx
+++ frontend/src/components/restaurants/Popular.tsx
@@ -3,7 +3,7 @@ import DishesCards from "../DishesCards";
 
 const Popular = () => {
   return (
-    <div className="container !mt-[100px] md:!mt-[150px]">
+    <div className="container !mt-[100px] md:!mt-[150px] custom-border-b">
       <h3 className="text-[24px] sm:text-[35px] font-semibold leading-[18px] sm:leading-[25px] text-[rgb(50,49,66)]">
         Popular 🔥
       </h3>
