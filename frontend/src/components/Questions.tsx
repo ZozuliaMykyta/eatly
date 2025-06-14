f5ab6947ba8004eee0499199ab5d4bb698012d02
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-06-14 22:08:52 +0200

    completed the questions section

--- frontend/src/components/Questions.tsx
+++ frontend/src/components/Questions.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import React, { useState } from "react";
 import { FaMinus, FaPlus } from "react-icons/fa";
 
@@ -45,30 +46,54 @@ const Questions = () => {
     }));
   };
   return (
-    <div className="container !mt-[120px]">
-      <h2 className="text-[32px] leading-[120%] capitalize text-center">
+    <div className="container !mt-[120px] text-center">
+      <h2 className="relative inline-block text-[32px] leading-[120%] capitalize text-center">
         frequently asked
         <br />
         <span className="text-purple"> questions</span>
+        <Image
+          src="/assets/img/questions-lines.svg"
+          alt="decoration"
+          width={41}
+          height={39}
+          className="absolute top-[-40px] right-[-50px]"
+        ></Image>
       </h2>
-      <div className="mt-[80px]">
+      <div className="mt-[80px] text-left">
         {questionsData.map((data) => (
-          <div key={data.id}>
-            <h3 className="" onClick={() => toggleList(data.id)}>
+          <div
+            key={data.id}
+            className="relative custom-border-b !pb-[40px] mb-[30px] last:mb-0"
+          >
+            <h3
+              className="cursor-pointer text-[23px] leading-[60px] text-[rgb(50,49,66)]"
+              onClick={() => toggleList(data.id)}
+            >
               {data.question}
             </h3>
             <p
               className={`
-                overflow-hidden transition-all duration-300 ease-in-out
+                overflow-hidden transition-all duration-300 ease-in-out text-[20px] leading-[160%] font-normal text-[rgb(104,104,104)]
                 ${
                   openedList[data.id]
-                    ? "max-h-96 opacity-100 scale-100 pointer-events-auto"
-                    : "max-h-0 opacity-0 scale-95 pointer-events-none"
+                    ? "max-h-96 opacity-100 scale-100 pointer-events-auto mt-5"
+                    : "max-h-0 opacity-0 scale-95 pointer-events-none mt-0"
                 }
               `}
             >
               {data.answer}
             </p>
+            <div className="absolute top-[16px] right-[13px]">
+              <div className="relative w-[30px] h-[30px] bg-purple rounded-full">
+                <div className="absolute top-[50%] left-[50%] translate-[-50%]">
+                  {openedList[data.id] ? (
+                    <FaMinus className="text-[12px]" color="white" />
+                  ) : (
+                    <FaPlus className="text-[12px]" color="white" />
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
