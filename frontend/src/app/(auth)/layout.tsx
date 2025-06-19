301f9ca9cae42404ffe7232bfefe64d20f349460
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-06-19 18:14:17 +0200

    fixed

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,7 +1,29 @@
+import StoreProvider from "../StoreProvider";
+import { Poppins, Inter, Manrope, Roboto } from "next/font/google";
 import type { Metadata } from "next";
+import "../globals.css";
 import React from "react";
-import MainLayout from "../shared/MainLayout";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--font-poppins",
+});
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-inter",
+});
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-manrope",
+});
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
 export const metadata: Metadata = {
   title: "Eatly",
   icons: {
@@ -14,5 +36,18 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <MainLayout>{children}</MainLayout>;
+  // return <MainLayout>{children}</MainLayout>;
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} ${inter.variable} ${manrope.variable} ${roboto.variable} font-poppins font-semibold`}
+      >
+        <StoreProvider>
+          <div className="wrapper">
+            <main>{children}</main>
+          </div>
+        </StoreProvider>
+      </body>
+    </html>
+  );
 }
--- frontend/src/app/shared/MainLayout.tsx
+++ frontend/src/app/shared/MainLayout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Poppins, Inter, Manrope, Roboto } from "next/font/google";
 import "../globals.css";
 import Header from "@/components/Header";
