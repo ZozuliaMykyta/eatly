9813abe66d70d4933c2600c8fee209a2dfd4e78a
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-08-02 16:09:16 +0200

    created Article and Articles pages and added them to the Blog page

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -1,3 +1,4 @@
+import Articles from "@/components/blog/Articles";
 import Discount from "@/components/Discount";
 import React from "react";
 
@@ -5,6 +6,7 @@ const page = () => {
   return (
     <>
       <Discount uniqueMargin={true} />
+      <Articles />
     </>
   );
 };
---
+++ frontend/src/components/blog/Articles.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Articles = () => {
+  return (
+    <div className="container">
+      <h2>
+        Latest <span className="text-purple">Articles</span>
+      </h2>
+    </div>
+  );
+};
+
+export default Articles;
