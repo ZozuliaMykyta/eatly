75005dd07b0ad10698afec7af3769c171599e6cb
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-08-10 18:13:26 +0200

    added swiper to Articles

--- frontend/src/components/blog/Articles.tsx
+++ frontend/src/components/blog/Articles.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { useGetArticlesQuery } from "@/lib/services/api";
+import { Swiper, SwiperSlide } from "swiper/react";
 import React from "react";
 import Article from "./ArticleCard";
+import { Navigation } from "swiper/modules";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa6";
 
 const Articles: React.FC = () => {
   const { data, error, isLoading } = useGetArticlesQuery();
@@ -10,12 +13,44 @@ const Articles: React.FC = () => {
       <h2 className="text-center text-[45px] leading-[120%]">
         Latest <span className="text-purple">Articles</span>
       </h2>
-      <div>
+      <Swiper
+        modules={[Navigation]}
+        navigation={{
+          nextEl: ".article-next",
+          prevEl: ".article-prev",
+        }}
+        spaceBetween={46}
+        slidesPerView={3}
+        // onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
+        grabCursor={true}
+        className="justify-center items-center text-center"
+        // breakpoints={{
+        //   320: {
+        //     slidesPerView: 1,
+        //     spaceBetween: 32,
+        //   },
+        //   600: {
+        //     slidesPerView: 1.3,
+        //   },
+        //   1024: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 46,
+        //   },
+        // }}
+      >
         {data &&
           data.map((article) => (
-            <Article article={article} key={article._id} />
+            <SwiperSlide key={article._id}>
+              <Article article={article} />
+            </SwiperSlide>
           ))}
-      </div>
+        <div className="article-prev">
+          <FaArrowLeft />
+        </div>
+        <div className="article-next">
+          <FaArrowRight />
+        </div>
+      </Swiper>
     </div>
   );
 };
