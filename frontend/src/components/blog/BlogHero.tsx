2264840bcd9feb862abe28beed0fa0dca0452751
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-08-24 19:41:26 +0200

    redesign BlogHero with author section and better layout

--- frontend/src/components/blog/BlogHero.tsx
+++ frontend/src/components/blog/BlogHero.tsx
@@ -2,6 +2,7 @@
 import { useGetArticlesQuery } from "@/lib/services/api";
 import Image from "next/image";
 import React from "react";
+import personAvatar from "@/assets/img/blog/icons/articlePerson.svg";
 
 const BlogHero = ({ slug }: { slug: string }) => {
   const { data, error, isLoading } = useGetArticlesQuery();
@@ -26,22 +27,27 @@ const BlogHero = ({ slug }: { slug: string }) => {
   }
 
   return (
-    <div className="container">
+    <div className="container !mt-[126px]">
       <h1 className="capitalize text-[37px] ">how to order good in eatly?</h1>
-      <div>
-        <div className="relative h-[250px] aspect-[310/272]">
-          <Image
-            src={article.img}
-            alt="Article's image"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 310px"
-            fill
-            className="object-cover"
-          />
-        </div>
+      <div className="flex items-center gap-2.5 mt-8">
+        <Image src={personAvatar} alt="Person's avatar" />
         <div>
-          <span>Written By</span>
-          <h6>Perperzon</h6>
+          <span className="text-[rgb(141,141,141)] text-[12px] font-normal leading-[18px]">
+            Written By
+          </span>
+          <h6 className="text-black text-[17px] leading-[26px] font-medium">
+            Perperzon
+          </h6>
         </div>
+      </div>
+      <div className="relative h-[250px] aspect-[310/272] mt-[62px]">
+        <Image
+          src={article.img}
+          alt="Article's image"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 310px"
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
   );
