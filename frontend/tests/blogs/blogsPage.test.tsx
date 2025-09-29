ab9e3c54f4135e7c6d2ffbc56c7647880a85950c
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-09-29 10:46:32 +0200

    test: add initial unit tests for Blogs Page component

---
+++ frontend/tests/blogs/blogsPage.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import Page from "@/app/blogs/[slug]/page";
+import { beforeEach, describe, expect, it } from "vitest";
+
+describe("Blogs Page", () => {});
