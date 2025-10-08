24887e227adf7f13a8006b27ee36051b20753b83
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-10-08 09:25:58 +0200

    feat: add pricing layout and plans components

---
+++ frontend/src/app/pricing/page.tsx
@@ -0,0 +1,12 @@
+import PricingPlans from "@/components/PricingPlans";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container">
+      <PricingPlans />
+    </div>
+  );
+};
+
+export default page;
