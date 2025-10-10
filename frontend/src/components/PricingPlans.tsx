876c46ac1d253d36c0f8b7f3389bfeb2d0185f8e
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-10-10 16:45:59 +0200

    feat: implement a basic responsive design

--- frontend/src/components/PricingPlans.tsx
+++ frontend/src/components/PricingPlans.tsx
@@ -1,8 +1,16 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaDollarSign } from "react-icons/fa";
 
 const PricingPlans = () => {
+  const [isWide, setIsWide] = useState(false);
+  useEffect(() => {
+    const checkWidth = () => setIsWide(window.innerWidth > 1024);
+    checkWidth();
+    window.addEventListener("resize", checkWidth);
+    return () => window.removeEventListener("resize", checkWidth);
+  }, []);
   const features = [
     {
       id: 1,
@@ -48,123 +56,138 @@ const PricingPlans = () => {
     },
   ];
   return (
-    <div className="relative rounded-[15px] bg-[#F9F9F9] pl-[66px] py-[54px] pr-[92px] mt-[66px]">
-      <div
-        className="absolute inset-0 -z-10"
-        style={{
-          borderRadius: "20.116px",
-          background: "#323142",
-          opacity: 0.15,
-          filter: "blur(92.19711303710938px)",
-        }}
-      />
-      <div className="flex justify-between items-center gap-10 mb-[40px]">
-        <div className="flex-[47.5%]">
-          <h2 className="text-[40px] text-[#282828] leading-[120%] mb-[25px]">
-            Pricing
-          </h2>
-          <h6 className="text-[#6E757C] text-[16px] font-inter font-medium leading-[24px]">
-            Affordable Basic& Pro Plans
-          </h6>
-        </div>
-        <div className="flex justify-between items-center gap-12 flex-[52.5%]">
+    <div>
+      {isWide ? (
+        <div className="relative rounded-[15px] bg-[#F9F9F9] pl-[66px] py-[54px] pr-[92px] mt-[66px]">
+          <div
+            className="absolute inset-0 -z-10"
+            style={{
+              borderRadius: "20.116px",
+              background: "#323142",
+              opacity: 0.15,
+              filter: "blur(92.19711303710938px)",
+            }}
+          />
+          <div className="flex justify-between items-center gap-10 mb-[40px]">
+            <div className="flex-[47.5%]">
+              <h2 className="text-[40px] text-[#282828] leading-[120%] mb-[25px]">
+                Pricing
+              </h2>
+              <h6 className="text-[#6E757C] text-[16px] font-inter font-medium leading-[24px]">
+                Affordable Basic& Pro Plans
+              </h6>
+            </div>
+            <div className="flex justify-between items-center gap-12 flex-[52.5%]">
+              <div>
+                <h3 className="text-[35px] text-[#282828] leading-[120%] mb-[30px]">
+                  Basic
+                </h3>
+                <h6 className="text-[#6E757C] text-[16px] font-inter font-medium leading-[24px]">
+                  Completely 100% Free Plan
+                </h6>
+              </div>
+              <div>
+                <h3 className="text-[35px] text-[#282828] leading-[120%] mb-[30px]">
+                  Premium
+                </h3>
+                <h6 className="text-[#6E757C] text-[16px] font-inter font-medium leading-[24px]">
+                  Amazing Premium Features Plan
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between items-center border-[#CBCBCB] border-t-1 border-b-1 py-[40px]">
+            <div className="flex-[50%]"></div>
+            <div className="flex gap-12 flex-[52.5%] justify-start">
+              <div className="flex items-center flex-[50%]">
+                <FaDollarSign className="text-[48px] text-[#272E35]" />
+                <span className="text-[64px] text-[#272E35] leading-[72px]">
+                  0
+                </span>
+                <span className="font-inter text-[16px] text-[#6E757C] font-medium leading-[24px] self-end mb-2">
+                  /month
+                </span>
+              </div>
+              <div className="flex items-center flex-[50%]">
+                <FaDollarSign className="text-[48px] text-[#272E35]" />
+                <span className="text-[64px] text-[#282828] leading-[72px]">
+                  5
+                </span>
+                <span className="font-inter text-[16px] text-[#6E757C] font-medium leading-[24px] self-end mb-2">
+                  /month
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="mt-[50px] pb-[64px] border-b-1">
+            <h4 className="mb-[50px] text-[24px] leading-[32px] text-[#272E35]">
+              Core features
+            </h4>
+            <div className="flex justify-between items-center gap-10 mt-[50px]">
+              <div className="flex-[47.5%]">
+                {features.map((feature) => (
+                  <h3
+                    key={feature.id}
+                    className="text-[#606060] text-[22px] font-medium leading-[32px] not-last:mb-[39px]"
+                  >
+                    {feature.name}
+                  </h3>
+                ))}
+              </div>
+              <div className="flex justify-between items-center gap-12 flex-[52.5%]">
+                <div className="flex flex-col flex-[50%]">
+                  {features.map((feature) => (
+                    <Image
+                      key={feature.id}
+                      src={feature.iconBasic}
+                      alt={feature.name}
+                      width={35}
+                      height={35}
+                      className="not-last:mb-[35px]"
+                    />
+                  ))}
+                </div>
+                <div className="flex flex-col flex-[50%]">
+                  {features.map((feature) => (
+                    <Image
+                      key={feature.id}
+                      src={feature.iconPremium}
+                      alt={feature.name}
+                      width={35}
+                      height={35}
+                      className="not-last:mb-[35px]"
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
           <div>
-            <h3 className="text-[35px] text-[#282828] leading-[120%] mb-[30px]">
-              Basic
-            </h3>
-            <h6 className="text-[#6E757C] text-[16px] font-inter font-medium leading-[24px]">
-              Completely 100% Free Plan
-            </h6>
-          </div>
-          <div>
-            <h3 className="text-[35px] text-[#282828] leading-[120%] mb-[30px]">
-              Premium
-            </h3>
-            <h6 className="text-[#6E757C] text-[16px] font-inter font-medium leading-[24px]">
-              Amazing Premium Features Plan
-            </h6>
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-between items-center border-[#CBCBCB] border-t-1 border-b-1 py-[40px]">
-        <div className="flex-[50%]"></div>
-        <div className="flex gap-12 flex-[52.5%] justify-start">
-          <div className="flex items-center flex-[50%]">
-            <FaDollarSign className="text-[48px] text-[#272E35]" />
-            <span className="text-[64px] text-[#272E35] leading-[72px]">0</span>
-            <span className="font-inter text-[16px] text-[#6E757C] font-medium leading-[24px] self-end mb-2">
-              /month
-            </span>
-          </div>
-          <div className="flex items-center flex-[50%]">
-            <FaDollarSign className="text-[48px] text-[#272E35]" />
-            <span className="text-[64px] text-[#282828] leading-[72px]">5</span>
-            <span className="font-inter text-[16px] text-[#6E757C] font-medium leading-[24px] self-end mb-2">
-              /month
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="mt-[50px] pb-[64px] border-b-1">
-        <h4 className="mb-[50px] text-[24px] leading-[32px] text-[#272E35]">
-          Core features
-        </h4>
-        <div className="flex justify-between items-center gap-10 mt-[50px]">
-          <div className="flex-[47.5%]">
-            {features.map((feature) => (
-              <h3
-                key={feature.id}
-                className="text-[#606060] text-[22px] font-medium leading-[32px] not-last:mb-[39px]"
-              >
-                {feature.name}
-              </h3>
-            ))}
-          </div>
-          <div className="flex justify-between items-center gap-12 flex-[52.5%]">
-            <div className="flex flex-col flex-[50%]">
-              {features.map((feature) => (
-                <Image
-                  key={feature.id}
-                  src={feature.iconBasic}
-                  alt={feature.name}
-                  width={35}
-                  height={35}
-                  className="not-last:mb-[35px]"
-                />
-              ))}
-            </div>
-            <div className="flex flex-col flex-[50%]">
-              {features.map((feature) => (
-                <Image
-                  key={feature.id}
-                  src={feature.iconPremium}
-                  alt={feature.name}
-                  width={35}
-                  height={35}
-                  className="not-last:mb-[35px]"
-                />
-              ))}
+            <div className="flex justify-between items-center mt-[44px]">
+              <div className="flex-[50%]"></div>
+              <div className="flex gap-12 flex-[52.5%] justify-start">
+                <div className="flex items-center flex-[50%]">
+                  <button className="text-[16px] font-medium leading-[0.488px] purple-btn rounded-xl py-7 px-6">
+                    start free
+                  </button>
+                </div>
+                <div className="flex items-center flex-[50%]">
+                  <button className="text-[16px] font-medium leading-[0.488px] purple-btn rounded-xl py-7 px-6">
+                    start Pro
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
-        <div className="flex justify-between items-center mt-[44px]">
-          <div className="flex-[50%]"></div>
-          <div className="flex gap-12 flex-[52.5%] justify-start">
-            <div className="flex items-center flex-[50%]">
-              <button className="text-[16px] font-medium leading-[0.488px] purple-btn rounded-xl py-7 px-6">
-                start free
-              </button>
-            </div>
-            <div className="flex items-center flex-[50%]">
-              <button className="text-[16px] font-medium leading-[0.488px] purple-btn rounded-xl py-7 px-6">
-                start Pro
-              </button>
-            </div>
-          </div>
+      ) : (
+        <div className="px-4 py-8">
+          <h2 className="text-2xl font-bold text-center mb-6">Pricing</h2>
+          <p className="text-center text-gray-600 mb-8">
+            Affordable Basic & Pro Plans
+          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
