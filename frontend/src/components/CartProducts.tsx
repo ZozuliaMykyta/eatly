a5a9f71cd7a3692a7843b226114b81ca17ebe608
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-10-24 16:00:55 +0200

    feat: enhance CartProducts component to display items in the cart

--- frontend/src/components/CartProducts.tsx
+++ frontend/src/components/CartProducts.tsx
@@ -1,24 +1,39 @@
+"use client";
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/lib/store";
 
 const CartProducts: React.FC = () => {
+  const items = useSelector((state: RootState) => state.cart.items);
+
   return (
     <div>
       <div>
-        <div>
-          <img src="" alt="" />
-          <div>
-            <h4></h4>
-            <h6></h6>
-          </div>
-        </div>
-        <div>
-          <div>
-            <button>-</button>
-            <span></span>
-            <button>+</button>
-          </div>
-          <h6></h6>
-        </div>
+        {items.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          <ul>
+            {items.map((item) => (
+              <li key={item._id}>
+                <div>
+                  <img src={item.image} alt={item.title} />
+                  <div>
+                    <h4>{item.title}</h4>
+                    <h6>{item.price}</h6>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <button>-</button>
+                    <span></span>
+                    <button>+</button>
+                  </div>
+                  <h6></h6>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
