5d3c1538c008519098f5486a1e7d253ae71c8f3f
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-10-24 08:58:03 +0200

    add removeItem and calculateTotal

--- frontend/src/lib/cart/cartSlice.ts
+++ frontend/src/lib/cart/cartSlice.ts
@@ -30,9 +30,21 @@ const cartSlice = createSlice({
       } else {
         state.items.push({ ...action.payload, quantity: 1 });
       }
-      //   cartSlice.caseReducers.calculateTotal(state)
+      cartSlice.caseReducers.calculateTotal(state);
+    },
+    removeItem: (state, action: PayloadAction<string>) => {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+      cartSlice.caseReducers.calculateTotal(state);
+    },
+    calculateTotal: (state) => {
+      const subTotal = state.items.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+      state.totalPrice = subTotal;
     },
   },
 });
 
+export const { addItem, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
