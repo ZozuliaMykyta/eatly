70e2e8a1ae9e6c123ce5f80f655fd67f708ce9ef
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2025-11-08 21:37:30 +0100

    fix: update Google OAuth callback URL configuration for improved flexibility

--- backend/auth/google.ts
+++ backend/auth/google.ts
@@ -8,7 +8,11 @@ import { v4 as uuidv4 } from "uuid";
 const options = {
   clientID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
   clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || "",
-  callbackURL: `${process.env.NEXT_PUBLIC_BE_BASE_URL}/api/auth/google/callback`,
+  callbackURL: `${
+    process.env.BACKEND_URL ||
+    process.env.NEXT_PUBLIC_BE_BASE_URL ||
+    "http://localhost:5000"
+  }/api/auth/google/callback`,
 };
 
 async function verify(
